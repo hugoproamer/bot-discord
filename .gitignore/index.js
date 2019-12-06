045c8cf5f9dbb3cb42892fc3e55b9a4b400ff886
hugoproamer <58593348+hugoproamer@users.noreply.github.com>
2019-12-06 12:17:05 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+var prefix = "!";
+
+const fs = require('fs');
+
+
+
+client.on("ready", () => {
+    console.log("je suis pret !");
+});
+
+
+
+
+client.login("NjUwMTA2MjQ0OTQ5OTk5NjI3.XeGkqg.keZn8D8TMwDbg6_s-3h8UgkEEDA");
